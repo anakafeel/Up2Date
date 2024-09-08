92eac878a8c0f46a52a6e9768d1227ccb3d669d5
Saim <hashmisaim037@gmail.com>
2024-09-07 22:48:36 -0400

    added cards and implemented rows and columns

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
-
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>THis is a news component meow meow
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>Up2Date Recent Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="My Title" description="My description" imageURL ="/" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My Title" description="My description" imageURL ="/" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="My Title" description="My description" imageURL ="/" />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
   render() {
+    const { title, description,imageUrl } = this.props;
     return (
-      <div>THis is news item</div>
+      <div>
+        <div class="card" style={{width: "18rem"}}>
+  <img src={imageUrl} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">{title}</h5>
+    <p class="card-text">{description}</p>
+    <a href="/" class="btn btn-outline-primary ">Read More</a>
+  </div>
+</div>
+      </div>
     )
   }
 }
