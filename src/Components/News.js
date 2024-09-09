a61c08a76e44ec949ddd04baf449ef6709653758
Saim <hashmisaim037@gmail.com>
2024-09-09 18:53:02 -0400

    Added animations with Framer

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,71 +1,114 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import { motion } from "framer-motion";
 
 export class News extends Component {
-
-
   /* METHOD - 1 -> COMPONENT DID MOUNT */
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       articles: [],
       loading: true,
-      page:1
-    }
+      page: 1,
+    };
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let jsondata = await data.json();
     console.log(jsondata);
-    this.setState({articles: jsondata.articles, totalArticles: jsondata.totalResults})
+    this.setState({
+      articles: jsondata.articles,
+      totalArticles: jsondata.totalResults,
+    });
   }
- 
-  nextpagehandler= async ()=>{
+
+  nextpagehandler = async () => {
     console.log("Next");
-    if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
-
-    }else{
-
-      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let jsondata = await data.json();
       console.log(jsondata);
       this.setState({
         page: this.state.page + 1,
-        articles: jsondata.articles
-      })
+        articles: jsondata.articles,
+      });
     }
+  };
 
-  }
-
-  previouspagehandler= async ()=>{
-    console.log("Previous")
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+  previouspagehandler = async () => {
+    console.log("Previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let jsondata = await data.json();
     console.log(jsondata);
     this.setState({
       page: this.state.page - 1,
-      articles: jsondata.articles
-    })
-  }
+      articles: jsondata.articles,
+    });
+  };
 
   render() {
     return (
       <div className="container my-3">
-        <h2 className ="text-center">Up2Date Top Recent Headlines: </h2>
+        <motion.h2
+          className="text-center"
+          initial={{ y: -200 }}
+          animate={{ y: 0 }}
+          transition={{ type:"spring", delay:0.5, duration:1 }}
+        >
+          Up2Date Top Recent Headlines:{" "}
+        </motion.h2>
         <div className="row">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-3" key={element.url}>
-            <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl ={element.urlToImage} newsUrl={element.url} />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <motion.div
+              initial={{x:-2000}}
+              animate={{x:0}}
+/*               whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }} */
+              transition={{type:"spring",delay:0.5 }}
+              className="col-md-3 my-5 " key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </motion.div>
+            );
           })}
         </div>
         <div className="container d-flex justify-content-end ">
-        <button disabled ={this.state.page<=1} type="button" className="btn btn-outline-danger btn-lg mx-2 my-2" onClick={this.previouspagehandler}>Previous Page</button> 
-        <button type="button" disabled ={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-outline-primary btn-lg my-2" onClick={this.nextpagehandler}>Next Page</button>
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-outline-danger btn-lg mx-2 my-2"
+            onClick={this.previouspagehandler}
+          >
+            Previous Page
+          </button>
+          <button
+            type="button"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            className="btn btn-outline-primary btn-lg my-2"
+            onClick={this.nextpagehandler}
+          >
+            Next Page
+          </button>
         </div>
       </div>
     );
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,22 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { motion } from "framer-motion";
 
 export class NewsItem extends Component {
-  
   render() {
-    const { title, description,imageUrl,newsUrl } = this.props;
+    const { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-        <img src={!imageUrl ? "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=1024x1024&w=is&k=20&c=-AdIwLn-nIYlgctc95CZuJHnkku_ia-f8A8m2LFwl2A=" : imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href={newsUrl} className="btn btn-outline-primary btn-lg">Read More</a>
-  </div>
-</div>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="card"
+          style={{ width: "18rem" }}
+        >
+          <img
+            src={
+              !imageUrl
+                ? "https://media.istockphoto.com/id/1311148884/vector/abstract-globe-background.jpg?s=1024x1024&w=is&k=20&c=-AdIwLn-nIYlgctc95CZuJHnkku_ia-f8A8m2LFwl2A="
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body ">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <motion.a
+              whileHover={{ opacity: 1 }}
+              initial={{ opacity: 0.5 }}
+              whileTap={{ scale: 0.9 }}
+              href={newsUrl}
+              className="btn btn-outline-primary btn-lg"
+            >
+              Read More
+            </motion.a>
+          </div>
+        </motion.div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
