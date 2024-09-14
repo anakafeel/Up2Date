510c56131b71b56e300989b7d2851f7582743a35
saim <hashmisaim037@gmail.com>
2024-09-14 00:11:47 -0400

    UpdateNews.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import { motion } from "framer-motion";
 import Loading from "./Loading";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 export class News extends Component {
-  static defaultProps ={
-    country: 'us',
+  static defaultProps = {
+    country: "us",
     pageSize: 8,
-    category: 'general',
-  }
+    category: "general",
+  };
 
-  static propTypes ={
+  static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-  }
+  };
 
   /* METHOD - 1 -> COMPONENT DID MOUNT */
   constructor() {
@@ -27,9 +27,9 @@ export class News extends Component {
     };
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let jsondata = await data.json();
     console.log(jsondata);
@@ -38,54 +38,20 @@ export class News extends Component {
       totalResults: jsondata.totalResults,
       loading: false,
     });
-    /* DEBUGGING */
-/*     console.log("Total Results: ", jsondata.totalResults); */
+  }
+
+  async componentDidMount() {
+    this.updateNews();
   }
 
   nextpagehandler = async () => {
-    /* DEBUGGING */
-/*     console.log("Next");
-    console.log("Page: ", this.state.page); // Log current page
-    console.log(
-      "Total Pages: ",
-      Math.ceil(this.state.totalResults / this.props.pageSize)
-    ); // Log calculated total pages */
-
-
-    /* NEW LOGIC */
-    if (
-      this.state.page + 1 <=
-      Math.ceil(this.state.totalResults / this.props.pageSize)
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true});
-      let data = await fetch(url);
-      let jsondata = await data.json();
-      console.log(jsondata);
-      this.setState({
-        page: this.state.page + 1,
-        articles: jsondata.articles,
-        loading: false,
-      });
-    }
+   this.setState({page: this.state.page + 1});
+   this.updateNews();
   };
 
   previouspagehandler = async () => {
-    console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${this.props.category}&apiKey=f24cdd8716d14d5baca39eaa037aae25&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
-    let data = await fetch(url);
-    let jsondata = await data.json();
-    console.log(jsondata);
-    this.setState({
-      page: this.state.page - 1,
-      articles: jsondata.articles,
-      loading: false,
-    });
+    this.setState({page: this.state.page - 1})
+    this.updateNews();
   };
 
   render() {
@@ -93,34 +59,35 @@ export class News extends Component {
       <div className="container my-3">
         <motion.h2
           className="text-center"
-          initial={{ y: -200}}
+          initial={{ y: -200 }}
           animate={{ y: 0 }}
           transition={{ type: "spring", delay: 0.5, duration: 1 }}
         >
           Up2Date Top Recent Headlines:{" "}
         </motion.h2>
-        {this.state.loading && <Loading/>}
+        {this.state.loading && <Loading />}
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <motion.div
-                initial={{ x: -2000 }}
-                animate={{ x: 0 }}
-                transition={{ type: "spring", delay: 0.5 }}
-                className="col-md-3 my-5 "
-                key={element.url}
-              >
-                <NewsItem
-                  title={element.title ? element.title : ""}
-                  description={element.description ? element.description : ""}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                />
-              </motion.div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <motion.div
+                  initial={{ x: -2000 }}
+                  animate={{ x: 0 }}
+                  transition={{ type: "spring", delay: 0.5 }}
+                  className="col-md-3 my-5 "
+                  key={element.url}
+                >
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                  />
+                </motion.div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-end ">
           <button
